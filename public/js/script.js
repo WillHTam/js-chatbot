11b0673f6c3e9f358e97d9cd74f39ee1189c196e
William T <william.tam18@gmail.com>
2017-08-18 01:36:15 +0800

    tiger balm really helps

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,19 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const recognition = new SpeechRecognition()
 const socket = io()
 const outputYou = document.querySelector('.output-you')
-cosnt outputBot = document.querySelector('.output-bot')
+const outputBot = document.querySelector('.output-bot')
 
 // set properties
 recognition.lang = 'en-US'
 recognition.interimResults = false
+recognition.maxAlternatives = 1
 
 document.querySelector('button').addEventListener('click', () => {
     recognition.start()
+})
+
+recognition.addEventListener('speechstart', () => {
+    console.log('Speech detected.')
 })
 
 recognition.addEventListener('result', (e) => {
@@ -21,3 +26,24 @@ recognition.addEventListener('result', (e) => {
     socket.emit('chat message', text)
 })
 
+recognition.addEventListener('speechend', () => {
+    recognition.stop()
+})
+
+recognition.addEventListener('error', (e) => {
+    outputBot.textContent = 'Error: ' + e.error
+})
+
+function synthVoice(text) {
+    const synth = window.speechSynthesis
+    const utterance = new SpeechSynthesisUtterance()
+    utterance.text = text
+    synth.speak(utterance)
+}
+
+socket.on('bot reply', function(replyText) {
+    synthVoice(replyText)
+
+    if (replyText == '') replyText = '(No answer...?)'
+    outputBot.textContent = replyText
+})
